247eb09009e30533be4b8c137c7a736bdd50dda8
Arkinul <45757532+Arkinul@users.noreply.github.com>
2022-11-13 23:29:43 +0100

    Issue 7, author needs last name

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -12,4 +12,9 @@ RSpec.describe Author, type: :model do
     author = Author.new(first_name: 'Alan', last_name: 'Turing', homepage: 'http://wikipedia.org/Alan_Turing')
     expect(author.name).to eq('Alan Turing')
   end
+  #author should not be valid without a last name
+  it "should not be valid without a last name" do
+    author = Author.new(first_name: 'Alan', homepage: 'http://wikipedia.org/Alan_Turing')
+    expect(author).to_not be_valid
+  end
 end
