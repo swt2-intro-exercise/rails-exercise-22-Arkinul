855af40ad675ad318e5ec5076153ad71e607ca37
Arkinul <45757532+Arkinul@users.noreply.github.com>
2022-11-14 08:48:52 +0100

    Issue 12,  error messages

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,4 +1,9 @@
 class Paper < ApplicationRecord
   #should always have title
   validates :title, presence: true, length: { minimum: 1 }
+  #should always have venue
+  validates :venue, presence: true, length: { minimum: 1 }
+  #should always have year and it should be an integer
+  validates :year, presence: true, numericality: { only_integer: true }
+
 end
